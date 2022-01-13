192088fce99fbc9b0b5acd35f50581fd531a825c
Kai Kuller <kavakai@gmail.com>
2022-01-13 11:52:48 -0700

    Add logo image, begin Promise

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,30 @@
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './domUpdates'
+import { userData, roomsData, allBookingsData } from "./apiCalls";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/image-from-rawpixel-id-3018024-png.png';
+import './images/turing-logo.png';
+import './images/kisspng-m-gustave-hotel-lobby-boy-5-lobby-boy-2-zero-bar-propaganda-5addf024831701.451758941524494372537.png'
+import User from './Classes/User';
 
+const checkIn = document.getElementById("checkIn")
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// Global Variables
+let currentUser;
+let currentRoom;
+let newBooking;
+let allUsers = [];
+let allRooms = [];
+let allBookings = [];
 
-// This is a test commit
+Promise.all([userData, roomsData, allBookingsData])
+    .then((data) => {
+        console.log(data, 'userData')
+        allUsers = data[0].customers.map(user => {
+            return new User(user.name, user.id)
+        })
+        currentUser = allUsers[0][Math.floor(Math.random() * allUsers.length)]
+        console.log(currentUser, 'user')
+    })
+
