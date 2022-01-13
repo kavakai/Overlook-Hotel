e6ba355056029270a66a3a2bf5d715c6eb1c21f5
Kai Kuller <kavakai@gmail.com>
2022-01-13 15:44:06 -0700

    Refactor Promise to get all data

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,9 @@
 const domUpdates = {
 
+    displayCurrentUserInfo(user) {
+        // display 'welcome user.name'
+        // past bookings => table with date of stay, room# 
+    }
 };
+
+export default domUpdates
--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,13 @@
 import './css/base.scss';
-import './domUpdates'
+import domUpdates from './domUpdates';
 import { userData, roomsData, allBookingsData } from "./apiCalls";
+import Room from './Classes/Room';
+import User from './Classes/User';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/image-from-rawpixel-id-3018024-png.png';
 import './images/turing-logo.png';
 import './images/kisspng-m-gustave-hotel-lobby-boy-5-lobby-boy-2-zero-bar-propaganda-5addf024831701.451758941524494372537.png'
-import User from './Classes/User';
 
 const checkIn = document.getElementById("checkIn")
 
@@ -20,10 +21,13 @@ let allBookings = [];
 
 Promise.all([userData, roomsData, allBookingsData])
     .then((data) => {
+        allBookings.push(data[2].bookings);
+        allRooms.push(data[1].rooms);
         allUsers = data[0].customers.map(user => {
             return new User(user)
-        })
-        currentUser = allUsers[Math.floor(Math.random() * allUsers.length)]
-        console.log(currentUser, 'user')
-    })
+        });
+        currentUser = allUsers[Math.floor(Math.random() * allUsers.length)];
+        allBookings.forEach(booking => currentUser.getAllBookings(booking));
+        domUpdates.displayCurrentUserInfo(currentUser)
+    });
 
