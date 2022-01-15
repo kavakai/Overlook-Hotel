c4923249c9c4f13f6a7ec2e18e02f21ed2c258e4
Kai Kuller <kavakai@gmail.com>
2022-01-15 14:09:27 -0700

    Pass hotel tests

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -116,54 +116,47 @@ describe('Hotel', function () {
                 roomServiceCharges: [],
             },
         ]);
+    });
 
         it('should have a list of all available rooms', function () {
             hotel.getAvailableRooms(date);
 
             expect(hotel.availableRooms).to.deep.equal([
-              {
-                id: "5fwrgu4i7k55hl6sz",
-                userID: 2,
-                date: "2022/04/22",
-                roomNumber: 15,
-                roomServiceCharges: [],
-              },
-              {
-                id: "5fwrgu4i7k55hl6t5",
-                userID: 3,
-                date: "2022/01/24",
-                roomNumber: 24,
-                roomServiceCharges: [],
-              },
-              {
-                id: "5fwrgu4i7k55hl6t6",
-                userID: 3,
-                date: "2022/01/10",
-                roomNumber: 12,
-                roomServiceCharges: [],
-              },
-              {
-                id: "5fwrgu4i7k55hl6t7",
-                userID: 1,
-                date: "2022/02/16",
-                roomNumber: 7,
-                roomServiceCharges: [],
-              },
-              {
-                id: "5fwrgu4i7k55hl6t8",
-                userID: 1,
-                date: "2022/02/05",
-                roomNumber: 12,
-                roomServiceCharges: [],
-              },
-              {
-                id: "5fwrgu4i7k55hl6w1",
-                userID: 1,
-                date: "2021/07/13",
-                roomNumber: 3,
-                roomServiceCharges: [],
-              },
+              [
+                {
+                  number: 7,
+                  roomType: "single room",
+                  bidet: false,
+                  bedSize: "queen",
+                  numBeds: 2,
+                  costPerNight: 231.46,
+                },
+                {
+                  number: 15,
+                  roomType: "residential suite",
+                  bidet: false,
+                  bedSize: "full",
+                  numBeds: 1,
+                  costPerNight: 294.56,
+                },
+                {},
+                {
+                  number: 12,
+                  roomType: "suite",
+                  bidet: true,
+                  bedSize: "king",
+                  numBeds: 2,
+                  costPerNight: 172.09,
+                },
+                {
+                  number: 3,
+                  roomType: "single room",
+                  bidet: false,
+                  bedSize: "king",
+                  numBeds: 1,
+                  costPerNight: 491.14,
+                },
+              ],
             ]);
         });
     });
-})
